4fa94259aa5a15b7e9f27a2c214856cc773b685c
Kakapulka <prettykilller@gmail.com>
2026-01-09 23:03:40 +0300

    logIn tmp

---
+++ cucu.js
@@ -0,0 +1,34 @@
+'use strict'
+console.log('hui');
+//alert("hello");
+//типы данных
+// 1.number
+let num = 10;
+let veryMuch = Infinity;
+let veryLittle = - Infinity;
+let huiPoimiShto = NaN;
+
+// 2. string
+let str = 'nu privet';
+let str2 = "privet2";
+let name = "sasha";
+let str3 = `privet zaebal, ${name}`;
+
+// 3. boolean
+let norm = true; //1, " ", -1
+let neNorm = false; //0, "", -0
+//console.log(Boolean(-1))
+
+// 4. null
+let neNol = null;
+
+// 5. undefined - переменная объявлена, но ей не присвоено никакого значения;
+let anonim = undefined;
+let age;
+console.log(age);
+
+// 6. object
+
+// 7. array
+
+typeof(norm); // "boolean"
